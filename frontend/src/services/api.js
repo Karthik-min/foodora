f605f4606390888ad5c3c64114e1d12b645fc38e
Hemanth Sheena <hemanthsheena24@gmail.com>
2026-06-03 10:19:10 +0530

    Fix Docker Compose and MongoDB container networking

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://13.62.228.227:5000/api';
 const IMAGE_BASE = API_URL.replace(/\/api\/?$/i, '');
 
 // Create axios instance
